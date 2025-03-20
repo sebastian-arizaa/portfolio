d6b4809052952f748b6990bbd6d8091183a09f51
sebastian ariza <sebadia1333@gmail.com>
2025-03-20 14:45:15 -0500

    Inicializacion del proyecto, dependencias y configuraciones iniciales

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  const aa = 'asdf'
+  console.log(aa, 'asfd')
+  return (
+    <>
+      <p className='bg-white'>Holaa</p>
+      <p className='bg-white'>Holaa</p>
+    </>
+  )
+}
+
+export default App
